d331f6a74e263c71b3042f4e18bb95498e0beae9
morepassionate <thisisthemylife@gmail.com>
2022-12-01 17:38:49 +0100

    fixed: pinia

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,27 +1,3 @@
-// import { createStore } from 'vuex'
-
-// import { getNFTCards } from '../api/apiCaller'
-
-// import { ICard } from '../interfaces'
-
-// export default createStore({
-//   state: {
-//     nftCardList: [] as ICard[],
-//   },
-//   mutations: {
-//     mutateNftCardList(state, payload: ICard[]) {
-//       state.nftCardList = [...state.nftCardList, ...payload]
-//     },
-//   },
-//   actions: {
-//     async mutateNftCardListAsync({ commit }, offset: number) {
-//       const { data } = await getNFTCards(offset)
-//       commit('mutateNftCardList', data.bundles)
-//     },
-//   },
-//   modules: {},
-// })
-
 import { defineStore } from 'pinia'
 
 import { getNFTCards } from '../api/apiCaller'
